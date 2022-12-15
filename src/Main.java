94d787da36158954941c850d63221813f692e30f
Your Name <you@example.com>
2022-12-15 18:27:57 +0200

    first commit

---
+++ src/Main.java
@@ -0,0 +1,31 @@
+import java.util.Random;
+
+public class Main {
+    public static void main(String[] args) {
+
+        Random random = new Random();
+        int[] firstTeam = new int[25];
+        for (int i = 0; i < firstTeam.length; i++) {
+            firstTeam[i] = random.nextInt(40 - 18) + 18;
+            System.out.print(firstTeam[i] + " ");
+
+        }
+        System.out.println();
+        int[] secondTeam = new int[25];
+        for (int i = 0; i < secondTeam.length; i++) {
+            secondTeam[i] = random.nextInt(40 - 18) + 18;
+            System.out.print(secondTeam[i] + " ");
+
+        }
+        System.out.println();
+        int middleAgeFirstTeam = 0;
+        for (int i = 0; i < firstTeam.length; i++) {
+            middleAgeFirstTeam += firstTeam[i];
+        }
+        int middleAgeSecondTeam = 0;
+        for (int i = 0; i < secondTeam.length; i++) {
+            middleAgeSecondTeam += secondTeam[i];
+                    }
+
+    }
+}
